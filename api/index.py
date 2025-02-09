2b5f88298e05d665b2739aff8eb57ce6b16fb1d6
Paritosh <79096636+paritosh142@users.noreply.github.com>
2025-02-09 18:18:46 +0530

    refactor: update CORS settings to allow all origins

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://byteupp.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
